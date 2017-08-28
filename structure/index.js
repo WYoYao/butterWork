f64f0b80121be3ddfacdde70001c3cb6e6fc097e
wyoyao <leosmilence@163.com>
2017-08-28 23:04:02 +0800

    tijiao

---
+++ structure/index.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+const path = require('path');
+
+// Load the full build.
+const _ = require('lodash');
+
+
+// 创建创建对应的文件结构
+function createStructure(map, p = __dirname) {
+
+
+
+
+}
+
+
+function _deep(attr) {
+    // if(_)
+}
+
+
+function isExists(url) {
+
+    return new Promise((resolve, reject) => {
+        // 保存链接
+        let uri = path.join(__dirname, url);
+        // 验证是否存在不存在创建对应的目录
+        fs.access(uri, err => !err ? resolve() : fs.mkdir(uri, () => resolve()));
+
+    });
+}
+
+let isPliuck = isExists('./publick');
+let isView=isExists('./View');
+
+
